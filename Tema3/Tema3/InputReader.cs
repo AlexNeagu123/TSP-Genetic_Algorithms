fa03ad2d7dc52f151b39c4a327af8aaecc786980
CristiFiodorov <cristifv59@gmail.com>
2022-12-12 21:45:09 +0200

    Added input reader

---
+++ Tema3/Tema3/InputReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3
+{
+	public class InputReader
+	{
+		public string FilePath { get; init; }
+		public List<(int id, (double x, double y) Coordonates)> Nodes;
+		
+		public InputReader(string filePath)
+		{
+			FilePath = filePath;
+		}
+
+		public void ReadInput()
+		{
+			string line;
+
+			using (StreamReader file = new System.IO.StreamReader(FilePath))
+			{
+				bool start = false;
+				while ((line = file.ReadLine()) != null)
+				{
+					if (line.Contains("EOF"))
+						break;
+
+
+					if (start)
+					{
+						var node = line.Split(' ');
+						Nodes.Add((int.Parse(node[0]), (double.Parse(node[1]), double.Parse(node[2]))));
+					}
+					else
+					{
+						if (line == "NODE_COORD_SECTION")
+						{
+							start = true;
+						}
+					}
+				}
+			}
+		}
+	}
+}
