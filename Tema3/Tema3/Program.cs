77d82db44fb95f74b846a0bf666939e9a7b758f1
CristiFiodorov <cristifv59@gmail.com>
2022-12-18 17:47:45 +0200

    Added HC

--- Tema3/Tema3/Program.cs
+++ Tema3/Tema3/Program.cs
@@ -9,11 +9,42 @@ namespace Tema3
 			var inputReader = new InputReader("InputFiles\\st70.tsp");
 			inputReader.ReadInput();
 
-			//var best = GeneticAlgorithm.Run(inputReader.Nodes, 2000, 200, 0.01, 0.90);
-			BaseSelection selection = new RouletteSelection();
-			BaseCrossover crossover = new PMXCrossover();
-			BaseMutation mutation = new IVMutation();
-			var best = GeneticAlgorithm.Run(inputReader.Nodes, 2000, 200, 0.01, 0.90, selection, mutation, crossover);
+			////var best = GeneticAlgorithm.Run(inputReader.Nodes, 2000, 200, 0.01, 0.90);
+			//BaseSelection selection = new RouletteSelection();
+			//BaseCrossover crossover = new PMXCrossover();
+			//BaseMutation mutation = new IVMutation();
+			//var best = GeneticAlgorithm.Run(inputReader.Nodes, 2000, 200, 0.01, 0.90, selection, mutation, crossover);
+
+			//foreach (var item in best.individ)
+			//{
+			//	Console.Write(item + " ");
+			//}
+
+			//Console.WriteLine();
+			//Console.WriteLine(best.value);
+
+
+			////Check If The Permutation Obtained Is Valid
+
+			//bool[] taken = new bool[best.individ.Length + 1];
+
+			//bool good = true;
+
+			//         foreach (var item in best.individ)
+			//         {
+			//	if (taken[item])
+			//		good = false;
+
+			//	taken[item] = true;
+			//         }
+
+			//if(good)
+			//	Console.WriteLine("Permutation Is Valid");
+			//else
+			//	Console.WriteLine("Permutation Is Invalid");
+
+
+			var best = HillClimbing.Run(inputReader.Nodes, 200, 2000);
 
 			foreach (var item in best.individ)
 			{
@@ -21,27 +52,8 @@ namespace Tema3
 			}
 
 			Console.WriteLine();
+
 			Console.WriteLine(best.value);
-
-
-			//Check If The Permutation Obtained Is Valid
-
-			bool[] taken = new bool[best.individ.Length + 1];
-			
-			bool good = true;
-
-            foreach (var item in best.individ)
-            {
-				if (taken[item])
-					good = false;
-
-				taken[item] = true;
-            }
-
-			if(good)
-				Console.WriteLine("Permutation Is Valid");
-			else
-				Console.WriteLine("Permutation Is Invalid");
-        }
+		}
 	}
 }
